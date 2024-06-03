7486ac5aa25e711ce62911bb6bb8b06820ebbfd0
denzonn <denson753@gmail.com>
2024-06-03 19:58:48 +0800

    New Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BE-2asW1.css') }}">
 </head>
 
 <body>
